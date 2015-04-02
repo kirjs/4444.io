45feefe928493bd609d3dd6f9ff368d3488b92a7
Kirill Cherkashin <kirill@kcherkashin.org>
2015-04-02 18:19:47 -0400

    Use spreadsheet as a data source to set up the redirects

---
+++ js/redirect.js
@@ -0,0 +1,39 @@
+//https://docs.google.com/spreadsheets/d/1vLwxHPStiqkVTWR_lU_gPAXaMS2kBTesNwyhy6wlUGE/pubhtml\
+
+
+function redirect( hash, matches ){
+    if (hash) {
+
+
+        var redirect = matches.reduce(function (result, redirect) {
+            var match = hash.match(redirect.match);
+            if (match) {
+                result = redirect.redirect.replace(/\$(\d)/, function (a, b) {
+                    return match[b];
+                });
+            }
+            return result;
+        }, undefined);
+
+        if (redirect) {
+            console.log(redirect);
+            //window.location = redirect;
+        }
+
+    }
+}
+
+
+function regexizeMatch( item ){
+    item.match = new RegExp(item.match);
+    return item;
+}
+
+Tabletop.init({
+    key: '1vLwxHPStiqkVTWR_lU_gPAXaMS2kBTesNwyhy6wlUGE',
+    callback: function (data, tabletop) {
+        redirect(location.hash.substr(1), data.map(regexizeMatch ))
+
+    },
+    simpleSheet: true
+});
